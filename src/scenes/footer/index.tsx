c1eb8697be6e18e48c058b977fc9f635bd8ce84e
Lance Turbes <instinct_truster@protonmail.com>
2023-05-22 12:13:54 -0700

    complete-footer

---
+++ src/scenes/footer/index.tsx
@@ -0,0 +1,33 @@
+import Logo from "@/assets/Logo.png";
+
+const Footer = () => {
+  return (
+    <footer className="bg-primary-100 py-16">
+      <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
+        <div className="mt-16 basis-1/2 md:mt-0">
+          <img src={Logo} alt="logo" />
+          <p className="my-5">
+            Add descriptive text here. Add descriptive text here. Add
+            descriptive text here. Add descriptive text here. Add descriptive
+            text here.
+          </p>
+          <p>(C) Evogym All Rights Reserved.</p>
+        </div>
+        <div className="mt-16 basis-1/4 md:mt-0">
+          <h4 className="font-bold">Links</h4>
+          <p className="my-5">Insert text here.</p>
+          <p className="my-5">Insert text here.</p>
+          <p>Insert text here.</p>
+        </div>
+        <div className="mt-16 basis-1/4 md:mt-0">
+          <h4 className="font-bold">Contact Us</h4>
+          <p className="my-5">Insert text here.</p>
+          <p className="my-5">Insert text here.</p>
+          <p>(333) 425-6825</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
